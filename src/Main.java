80fc6be0ee0b2a9f4aa0f598673417dbbde9d8ce
Anton <fynjycedjhjd@gmail.com>
2022-10-19 17:09:12 +0300

    сделал 4 задание

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = generateRandomArray();
-        int minCashe = 5;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < arr[i] + 1) {
-                minCashe = arr[i];
-            }
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
-        System.out.println(minCashe);
-
-    }
-
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-        }
-        return arr;
+        System.out.println(" переоформил дз");
     }
 }
